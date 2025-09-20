c307f2cac8a8e3ad093579dbca71312c6a6dc622
UdamShakya <udamilukpotha@gmail.com>
2025-09-20 23:42:37 +0530

    variables

--- basic.py
+++ basic.py
@@ -1,3 +1,14 @@
 # hello.py
 name = input("What is your name? ")
 print(f"Hello, {name}! Welcome to Python 🚀")
+
+
+# variables.py
+student_name = "Udam"
+age = 21
+subjects = ["Math", "Physics", "Computer Science"]
+
+print(f"{student_name} is {age} years old.")
+print("Subjects enrolled:")
+for subject in subjects:
+    print("-", subject)
